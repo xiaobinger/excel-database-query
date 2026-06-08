4bba808e49105b580a482d926139f3145f90199c
熊兵 <xiongbing@juheba.com>
2026-06-08 15:52:20 +0800

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    host: '192.168.3.221',
+    host: '0.0.0.0',
     port: 3000,
     proxy: {
       '/api': {
