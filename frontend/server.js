77e1ad74d607890173436db3d2bf521547d33262
熊兵 <xiongbing@juheba.com>
2026-06-08 15:36:11 +0800

    fix

--- frontend/server.js
+++ frontend/server.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 // SPA路由回退
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, '0.0.0.0', () => {
